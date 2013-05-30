1db9376a369f15568d3a2edf4c49dafeeb369432
Tankovski <stoian.tankovski@gmail.com>
2013-05-31 01:07:05 +0300

    Add exercises to LinearDataStructuresHomework

---
+++ SDA/LinearDataStructures/Homework/First50MembersOfASequence/First50MembersOfASequence.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class First50MembersOfASequence
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number:");
+        string input = Console.ReadLine();
+        int number;
+        bool isNumber = int.TryParse(input, out number);
+
+        if (!isNumber)
+        {
+            throw new ArgumentException("This is not a number");
+        }
+
+        List<int> first50Numbers = Add50Elements(number);
+
+        for (int i = 0; i < 50; i++)
+        {
+            Console.Write(first50Numbers[i]+" ");
+        }
+        Console.WriteLine();
+    }
+
+    private static List<int> Add50Elements(int number)
+    {
+        Queue<int> sequence = new Queue<int>();
+        List<int> first50Numbers = new List<int>();
+        sequence.Enqueue(number);
+        first50Numbers.Add(number);
+        int count = 1;
+        while (first50Numbers.Count < 50)
+        {
+            int S1 = sequence.Dequeue();
+            int S2 = S1 + 1;
+            int S3 = 2 * S1 + 1;
+            int S4 = S1 + 2;
+
+            sequence.Enqueue(S2);
+            sequence.Enqueue(S3);
+            sequence.Enqueue(S4);
+
+            first50Numbers.Add(S2);
+            first50Numbers.Add(S3);
+            first50Numbers.Add(S4);
+
+            count++;
+        }
+
+        return first50Numbers;
+    }
+}
+
---
+++ SDA/LinearDataStructures/Homework/ImplementationOfLinkedList/OurLinkedList.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementationOfLinkedList
+{
+    class OurLinkedList<T>
+    {
+        private ListItem<T> firstElement;
+
+        public int Count
+        {
+            get
+            {
+                int count = CalculateCount();
+                return count;
+            }
+            private set { }
+        }
+
+        public ListItem<T> FirstElement
+        {
+            get
+            {
+                return this.firstElement;
+            }
+            set
+            {
+                this.firstElement = value;
+            }
+        }
+
+        public OurLinkedList()
+        {
+            this.FirstElement = null;
+        }
+
+        public void AddFirst(T value)
+        {
+            if (this.FirstElement==null)
+            {
+                this.FirstElement = new ListItem<T>(value);
+            }
+            else
+            {
+                ListItem<T> newListItem = new ListItem<T>(value);
+                newListItem.NextItem = this.FirstElement;
+                this.FirstElement = newListItem;
+            }
+        }
+
+        public void AddLast(T value)
+        {
+            if (this.FirstElement==null)
+            {
+                this.FirstElement = new ListItem<T>(value);
+            }
+            else
+            {
+                ListItem<T> next = this.FirstElement;
+
+                while (next.NextItem!=null)
+                {
+                    next = next.NextItem;
+                }
+
+                next.NextItem = new ListItem<T>(value);
+            }
+        }
+
+        //AddAfter and AddBefore dont work very good becouse they taka every ListItem thah you give like parameter
+        public void AddAfter(ListItem<T> item,T value)
+        {
+            ListItem<T> next = this.FirstElement;
+            while (next != item && next != null)
+            {
+                next = next.NextItem;
+                if (next == null)
+                {
+                    throw new ArgumentException("The ListItem do not belong to OurLinkedList!");
+                }
+            }
+            ListItem<T> newItem = new ListItem<T>(value);
+            newItem.NextItem = next.NextItem;
+            next.NextItem = newItem;
+        }
+
+        public void AddBefore(ListItem<T> item,T value)
+        {
+            if (item==this.FirstElement)
+            {
+                ListItem<T> newList = new ListItem<T>(value);
+                newList.NextItem = this.FirstElement;
+                this.FirstElement = newList;
+            }
+            else
+            {
+                ListItem<T> next = this.FirstElement;
+
+                while (next.NextItem!=item)
+                {
+                    next = next.NextItem;
+                    if (next==null)
+                    {
+                         throw new ArgumentException("The ListItem do not belong to OurLinkedList!");
+                    }
+                }
+                
+                    ListItem<T> newList = new ListItem<T>(value);
+                    newList.NextItem = next.NextItem;
+                    next.NextItem = newList;
+                
+               
+            }
+        }
+
+        public void RemoveFirst()
+        {
+            this.FirstElement = this.FirstElement.NextItem;
+        }
+
+        public void RemoveLast()
+        {
+            ListItem<T> next = this.FirstElement;
+
+            while (next.NextItem !=  null)
+            {
+                next = next.NextItem;
+            }
+            ListItem<T> again = this.FirstElement;
+            while (again.NextItem!=next)
+            {
+                again = again.NextItem;
+            }
+            again.NextItem = null;
+        }
+
+        private int CalculateCount()
+        {
+            int count = 1;
+            ListItem<T> next = this.FirstElement;
+
+            while (next.NextItem != null)
+            {
+                next = next.NextItem;
+                count += 1;
+            }
+
+            return count;
+        }
+    }
+}
