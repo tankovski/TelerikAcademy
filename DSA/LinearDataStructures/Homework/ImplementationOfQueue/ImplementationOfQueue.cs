aac2732e3de79b89106e3aa0b7be7c9e0a83dd71
Tankovski <stoian.tankovski@gmail.com>
2013-06-02 23:31:48 +0300

    Add ImplementationOfQueue

---
+++ DSA/LinearDataStructures/Homework/ImplementationOfQueue/ImplementationOfQueue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementationOfQueue
+{
+    class ImplementationOfQueue
+    {
+        static void Main(string[] args)
+        {
+            OurQueue<int> queue = new OurQueue<int>();
+            queue.Enqueue(5);
+            queue.Enqueue(15);
+            queue.Enqueue(25);
+            queue.Enqueue(35);
+
+            int[] array = queue.ToArray();
+            Console.WriteLine(queue.Peek());
+            while (queue.Count>0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+            Console.WriteLine(queue.Contains(15));     
+        }
+    }
+}
---
+++ DSA/LinearDataStructures/Homework/ImplementationOfQueue/OurQueue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementationOfQueue
+{
+    class OurQueue<T>
+    {
+        private LinkedList<T> container;
+
+        public int Count
+        {
+            get
+            {
+                return this.container.Count;
+            }
+            private set
+            {
+            }
+        }
+
+        public OurQueue()
+        {
+            this.container = new LinkedList<T>();
+        }
+
+        public void Enqueue(T value)
+        {
+            this.container.AddLast(value);
+        }
+
+        public T Dequeue()
+        {
+            if (this.container.First==null)
+            {
+                throw new ArgumentNullException("OurQueue is empty!");
+            }
+            T value = this.container.First.Value;
+            this.container.RemoveFirst();
+
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (this.container.First == null)
+            {
+                throw new ArgumentNullException("OurQueue is empty!");
+            }
+            T value = this.container.First.Value;
+            
+            return value;
+        }
+
+        public bool Contains(T value)
+        {
+            bool isContainig = this.container.Contains(value); ;
+
+            return isContainig;
+        }
+
+        public T[] ToArray()
+        {
+           return this.container.ToArray();
+        }
+    }
+}
