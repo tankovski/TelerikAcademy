8ad7158628b82c41e6ab48a8b90b6b781acbeb4e
Tankovski <stoian.tankovski@gmail.com>
2013-06-03 18:11:32 +0300

    Add task to one homework and SliceAndDice folder
    Add the task for FavoriteSlideBar to JS-App advanced oop homework.
    Add SliceAndDice homeworks to the repository.

---
+++ SliceAndDice/Lesson5-HTML5-Everywhere/html5-everywhere-demos/html5-everywhere-demos/scripts/js-shims/index-of.js
@@ -0,0 +1,14 @@
+﻿(function () {
+	if (!Array.prototype.indexOf) {
+		function arrayIndexOf (searchElement, fromIndex) {
+			for (var i = fromIndex || 0, len = this.length; i < len; i++) {
+				if (this[i] === searchElement) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		Array.prototype.indexOf = arrayIndexOf;
+	}
+}).call(this);
