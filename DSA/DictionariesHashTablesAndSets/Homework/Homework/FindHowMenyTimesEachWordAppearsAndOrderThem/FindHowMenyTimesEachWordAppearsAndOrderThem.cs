7c7bdb80637ffa48d64fddddd87bef87e0aa7a5f
Tankovski <stoian.tankovski@gmail.com>
2013-06-06 08:57:59 +0300

    Optimization

--- DSA/DictionariesHashTablesAndSets/Homework/Homework/FindHowMenyTimesEachWordAppearsAndOrderThem/FindHowMenyTimesEachWordAppearsAndOrderThem.cs
+++ DSA/DictionariesHashTablesAndSets/Homework/Homework/FindHowMenyTimesEachWordAppearsAndOrderThem/FindHowMenyTimesEachWordAppearsAndOrderThem.cs
@@ -11,25 +11,23 @@ namespace FindHowMenyTimesEachWordAppearsAndOrderThem
         static void Main()
         {
             string text = "This is the TEXT. Text, text, text – THIS TEXT! Is this the text?";
-            string[] values = text.ToLower().Split(' ', '.', ',', '!', '–', '?');
-            SortedDictionary<string, int> dictionary = new SortedDictionary<string, int>();
+            char[] separators = { ' ', '.', ',', '!', '–', '?' };
+            string[] values = text.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
 
             foreach (var value in values)
             {
-                if (value != string.Empty)
+                int count = 0;
+                if (dictionary.ContainsKey(value))
                 {
-                    int count = 0;
-                    if (dictionary.ContainsKey(value))
-                    {
-                        count = dictionary[value];
-                    }
-                    dictionary[value] = count + 1;
+                    count = dictionary[value];
                 }
+                dictionary[value] = count + 1;
             }
 
             foreach (KeyValuePair<string, int> item in dictionary.OrderBy(key => key.Value))
             {
-                Console.WriteLine("{0} -> {1} times",item.Key,item.Value);
+                Console.WriteLine("{0} -> {1} times", item.Key, item.Value);
             }
         }
     }
