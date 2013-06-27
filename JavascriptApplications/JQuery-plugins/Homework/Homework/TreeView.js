b317b86aa2ce2049ab9e8e003e2bc8ea43756697
Tankovski <stoian.tankovski@gmail.com>
2013-06-27 22:23:20 +0300

    Upload JQuery-plugins homework

---
+++ JavascriptApplications/JQuery-plugins/Homework/Homework/TreeView.js
@@ -0,0 +1,19 @@
+﻿(function ($) {
+    $.fn.treeView = function () {
+        var elements = $(this.selector);
+        elements.on("mouseover", function () {
+            var oldWidth = parseInt($(this).css("width"));
+            var oldHeight = parseInt($(this).css("height"));
+            $(this).css("width", (oldWidth * 2) + "px");
+            $(this).css("height", (oldHeight * 2) + "px");
+        });
+
+        elements.on("mouseout", function () {
+            var oldWidth = parseInt($(this).css("width"));
+            var oldHeight = parseInt($(this).css("height"));
+            $(this).css("width", (oldWidth / 2) + "px");
+            $(this).css("height", (oldHeight / 2) + "px");
+        });
+        return this;
+    }
+}(jQuery))
