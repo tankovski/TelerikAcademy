75934bc2a59c4cddc41f12d258a9e5f34a459fb4
Tankovski <stoian.tankovski@gmail.com>
2013-09-09 11:08:53 +0300

    Upload Asp.Net intro

---
+++ Asp.NetWebForms/Intro/Homework/SumatorMvc/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SumatorMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AddTwoNumbers(double firstNumber, double secondNumber)
+        {
+            double result = firstNumber + secondNumber;
+            ViewBag.Result = result;
+
+            return View("Index");
+        }
+    }
+}
